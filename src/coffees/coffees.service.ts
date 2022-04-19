8704169fdb735f801ebe16c293afc375306268f8
Parker Chang <45290853+Parkerhiphop@users.noreply.github.com>
2022-04-19 17:10:27 +0800

    feat: using typeorm to implement coffee and flavor

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,52 +1,48 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 
-/**
- * @feature
- * Create a Simple Service & CRUD of coffee
- */
 @Injectable()
 export class CoffeesService {
-  // private: 只有在 Service 內能被存取
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
+  constructor(
+    // Using TypeORM "Repository" to Access Database
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
 
-  // 封裝 Business Logic： 讓 Controller 可以直接呼叫這個 function 就好
   findAll() {
-    return this.coffees;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: string) {
-    // throw 'Bubble up the random error'; // -> 501
-    const coffee = this.coffees.find((item) => item.id === +id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne(id);
     if (!coffee) {
-      // throw new HTTPException('Error Msg', HttpStatus.NOT_FOUND)
       throw new NotFoundException(`Coffee #${id} not found`);
     }
     return coffee;
   }
 
-  create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
   }
 
-  update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
-      // update the existing entity
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const coffee = await this.coffeeRepository.preload({
+      id: +id,
+      ...updateCoffeeDto,
+    });
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
+    return this.coffeeRepository.save(coffee);
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    return this.coffeeRepository.remove(coffee);
   }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,46 @@
-// 類似 interface
+// Creating a TypeORM Entity
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  // Create A Relation between two entities
+  @JoinTable() // 👈 Join the 2 tables - only the OWNER-side does this
+  @ManyToMany(
+    (type) => Flavor,
+    (flavor) => flavor.coffees, // what is "coffee" within the Flavor Entity
+    {
+      // Cascading Inserts (Add a new coffee with new flavor -> Create Flavor at the same time )
+      cascade: true, // 👈 or optionally just insert or update ['insert']
+    },
+  ) // 👈
   flavors: string[];
 }
+
+/*
+Generated Coffee table in PostgreSQL Database
+
++-------------+--------------+----------------------------+
+|                          coffee                         |
++-------------+--------------+----------------------------+
+| id          | int(11)      | PRIMARY KEY AUTO_INCREMENT |
+| name   	    | varchar      |                            |
+| brand       | varchar      |                            |
+| flavors     | json         |                            |
++-------------+--------------+----------------------------+
+*/
